f06ca1401ca14c71e48d936ac2a9fcbe7f6b46b4
Eduardo Craveiro <eduardo.craveiro26@gmail.com>
2026-08-27 10:26:49 -0300

    feat: Criando lista com botão adicionar e excluir

---
+++ src/components/Tarefas.jsx
@@ -0,0 +1,82 @@
+import { useState, useEffect } from 'react'
+
+const Tarefas = () => {
+
+    //HOOK-useState - manipula o estado da variavel e guarda os dados
+    const [tarefas, setTarefas] = useState(() => {
+        const salvarTarefas = localStorage.getItem("item-tarefa");
+        return salvarTarefas ? JSON.parse(salvarTarefas) : [];
+    });
+    //useState para manipular os dados que passar nos campos
+    const [campo, setCampo] = useState("");
+
+    //HOOK-useEffect- realiza um efeito colateral, no exemplo vai 
+    //carregar automaticamente as tarefas cadastradas.
+
+    useEffect(() => {
+        localStorage.setItem("item-tarefa", JSON.stringify(tarefas));
+    }, [tarefas])
+
+    //função adicionar tarefa
+
+    const AdicionarTarefa = (e) => {
+        e.preventDefault();
+        if (!campo.trim()) return;
+
+        const novaTarefa = {
+            id: Date.now(),
+            text: campo,
+        };
+
+        setTarefas([...tarefas, novaTarefa]);
+        setCampo();
+    };
+
+    const RemoverTarefa = (id) => {
+        const apagarTarefa = tarefas.filter((tarefa) => tarefa.id !== id);
+        setTarefas(apagarTarefa)
+    };
+
+    return (
+        <>
+            <div className="todo-container">
+                <h2>Minha Lista de Tarefas</h2>
+
+                <form onSubmit={AdicionarTarefa} className="todo-form">
+                    <input
+                        type="text"
+                        value={campo}
+                        onChange={(e) => setCampo(e.target.value)}
+                        placeholder="Digite uma nova tarefa..."
+                        className="todo-input"
+                    />
+                    <button type="submit" className="btn-adicionar">
+                        Adicionar
+                    </button>
+                </form>
+
+                <ul className="todo-lista">
+                    {tarefas.map((tarefa) => (
+                        <li key={tarefa.id} className="todo-item">
+                            <span>{tarefa.text}</span>
+                            {/* arrow function (função seta) que encapsula a execução de outra função. 
+            Ela garante que removerTarefa só seja executada quando o evento acontecer (como um clique de botão), 
+            e não assim que a página carregar.
+            */}
+                            <button onClick={() => RemoverTarefa(tarefa.id)}
+                                className="btn-delete"
+                            >
+                                Excluir
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+
+                {tarefas.length === 0 && <p className="mensagem">Nenhuma tarefa salva.</p>}
+            </div>
+
+        </>
+    )
+}
+
+export default Tarefas
